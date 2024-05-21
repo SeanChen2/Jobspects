e95c57c69daeb56f43d76ae525db23c862f0fdd2
Vantage11 <randiv.waduge@gmail.com>
2024-05-21 13:22:37 -0400

    Update UnemploymentDurationChartController.java

--- src/controller/UnemploymentDurationChartController.java
+++ src/controller/UnemploymentDurationChartController.java
@@ -1,39 +1,44 @@
 package controller;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-
-import javax.swing.JButton;
-
-public class UnemploymentDurationChartController extends ChartController implements ActionListener{
-	
-	JButton[] chartNavButtons;
-	
-	
-	public UnemploymentDurationChartController() {
-		super(menuFrame);
-
-		
-		
-
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		
-	}
-
-	@Override
-	public void updateChart() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	//wants it not to be void in chartController
-	public ArrayList<Double> getValuesForAverage() {
-		return null;
-		// TODO Auto-generated method stub
-	}
+public class UnemploymentDurationChartController {
 
 }
+
+
+//package controller;
+//
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.util.ArrayList;
+//
+//import javax.swing.JButton;
+//
+//public class UnemploymentDurationChartController extends ChartController implements ActionListener{
+//	
+//	JButton[] chartNavButtons;
+//	
+//	
+//	public UnemploymentDurationChartController() {
+//		
+//		
+//
+//	}
+//	
+//	public void actionPerformed(ActionEvent e) {
+//		
+//	}
+//
+//	@Override
+//	public void updateChart() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	//wants it not to be void in chartController
+//	public ArrayList<Double> getValuesForAverage() {
+//		return null;
+//		// TODO Auto-generated method stub
+//	}
+//
+//}
