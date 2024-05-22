c0574bf86dac01cdf0aae45e7179b5fb272ba7c0
Vantage11 <randiv.waduge@gmail.com>
2024-05-22 13:23:35 -0400

    Im doin pretty good
    rn

--- src/view/UnemploymentDurationLineChartFrame.java
+++ src/view/UnemploymentDurationLineChartFrame.java
@@ -5,15 +5,20 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import view.OccupationLabourBarChartFrame.CircularGradientPanel;
 
 public class UnemploymentDurationLineChartFrame extends JobspectsChartFrame implements ActionListener{
+	
+	private JPanel filterPanel = new JPanel();
 	
 	public UnemploymentDurationLineChartFrame() {
 		setSize(1920, 1080);
@@ -58,7 +63,8 @@ public class UnemploymentDurationLineChartFrame extends JobspectsChartFrame impl
 		getContentPane().setLayout(null);
 
 
-		// Create and add JTextArea
+		 //Create and add JTextArea
+		
 //		JTextArea textArea = new JTextArea();
 //		textArea.setFont(new Font("Serif", Font.PLAIN, 16));
 //		textArea.setForeground(Color.BLACK);
@@ -87,7 +93,20 @@ public class UnemploymentDurationLineChartFrame extends JobspectsChartFrame impl
 
 
 	private void setUpFilterPanel() {
-		// TODO Auto-generated method stub
+		//Display the filter "sections" vertically using a box layout
+		filterPanel.setLayout(new BoxLayout(filterPanel, BoxLayout.Y_AXIS));
+		filterPanel.setBounds(FRAME_WIDTH / 2 + 150, 180, FRAME_WIDTH / 2 - 200, FRAME_HEIGHT - 290);
+		filterPanel.setBackground(Color.decode("#fffef7"));
+
+		
+		//Make the filter panel vertically scrollable
+		JScrollPane filterScrollPane = new JScrollPane(filterPanel, 
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+	            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		
+		filterScrollPane.setBounds(FRAME_WIDTH / 2 + 150, 180, FRAME_WIDTH / 2 - 200, FRAME_HEIGHT - 290);
+		filterScrollPane.setBackground(Color.WHITE);
+		add(filterScrollPane);
 		
 	}
 
