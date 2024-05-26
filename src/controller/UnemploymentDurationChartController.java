db6a0a89283e25298c4dc12d26974090898e0e9e
Vantage11 <randiv.waduge@gmail.com>
2024-05-26 12:55:09 -0400

    test (old code)

--- src/controller/UnemploymentDurationChartController.java
+++ src/controller/UnemploymentDurationChartController.java
@@ -6,15 +6,27 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
 import view.JobspectsMenuFrame;
+import view.UnemploymentDurationLineChartFrame;
 
 public class UnemploymentDurationChartController extends ChartController implements ActionListener{
 	
 	JButton[] chartNavButtons;
 	
+	
 	public UnemploymentDurationChartController(JobspectsMenuFrame menuFrame) {
+		
 		super(menuFrame);
-		// TODO Auto-generated constructor stub
+		
+		setChartFrame(new UnemploymentDurationLineChartFrame());
+		showChartFrame();
+		
+		updateChart();
 	}
 	
 	public void actionPerformed(ActionEvent e) {
@@ -23,10 +35,19 @@ public class UnemploymentDurationChartController extends ChartController impleme
 
 	@Override
 	public void updateChart() {
-		// TODO Auto-generated method stub
+		
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(500, "sdfsdf", "2020");
+		dataset.addValue(111, "sdfsdf", "2024");
+		
+		dataset.addValue(520, "(trying again)", "1999");
+		dataset.addValue(1111, "(trying again)", "2024");
+		
+		setChart(ChartFactory.createLineChart("Placeholder", "Date", "Jobless induviduals", dataset, PlotOrientation.VERTICAL, true, true, false));
+		
 		
 	}
-
+	
 	@Override
 	//wants it not to be void in chartController
 	public ArrayList<Double> getValuesForAverage() {
--- src/view/UnemploymentDurationLineChartFrame.java
+++ src/view/UnemploymentDurationLineChartFrame.java
@@ -5,17 +5,20 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 
 import view.OccupationLabourBarChartFrame.CircularGradientPanel;
 
-public class UnemploymentDurationLineChartFrame extends JFrame implements ActionListener{
+public class UnemploymentDurationLineChartFrame extends JobspectsChartFrame implements ActionListener{
 	
-	private JButton backButton;
+	private JPanel filterPanel = new JPanel();	
 
 	public UnemploymentDurationLineChartFrame() {
 		setSize(1920, 1080);
@@ -24,7 +27,9 @@ public class UnemploymentDurationLineChartFrame extends JFrame implements Action
 		//CircularGradientPanel gradientPanel = new CircularGradientPanel();
 		//gradientPanel.setLayout(null);
 		//setContentPane(gradientPanel);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setAverageCalculationPanel(new AverageCalculationPanel(true));
 		
 		setUpChartNavButtons();
 		
@@ -36,35 +41,17 @@ public class UnemploymentDurationLineChartFrame extends JFrame implements Action
 		 
 		 SetUpAverageSection();
 		 
-		 getChartNavButtons();
-		 
-		 getCompareCategoryButtons();
-		 
-		 getSexButtons();
+//		 getChartNavButtons();
+//		 
+//		 getCompareCategoryButtons();
+//		 
+//		 getSexButtons();
 		 
 		// getTimeRangeTextFields()
 		
-		// Create button
-		backButton = new JButton("<");
-		backButton.addActionListener(this);
-
-		// Set absolute position for button
-		backButton.setBounds(10, 10, 50, 30); // (x, y, width, height)
-
-		// Add button to content pane
-		getContentPane().add(backButton);
-
 		// Create and add label
-		JLabel titleLabel = new JLabel("What factors affect the Duration of Employment in Canada?");
-		titleLabel.setFont(new Font("Serif", Font.BOLD, 55));
-		titleLabel.setForeground(Color.WHITE); // Set text color to white
-
-		// Set position for label
-		titleLabel.setBounds(10, 30, 5000, 100); // (x, y, width, height)
-
-		// Add label to content pane
-		getContentPane().add(titleLabel);
-
+		getScreenTitleLabel().setText("    What factors affect the Duration of Employment in Canada?");
+		
 		// Set content pane layout to null for absolute positioning
 		getContentPane().setLayout(null);
 
@@ -83,7 +70,7 @@ public class UnemploymentDurationLineChartFrame extends JFrame implements Action
 		// Set content pane layout to null for absolute positioning
 		getContentPane().setLayout(null);
 		
-		setVisible(false);
+		//setVisible(false);
 
 
 	}
@@ -112,8 +99,20 @@ public class UnemploymentDurationLineChartFrame extends JFrame implements Action
 
 
 	private void setUpFilterSection() {
-		// TODO Auto-generated method stub
+		//Display the filter "sections" vertically using a box layout
+		filterPanel.setLayout(new BoxLayout(filterPanel, BoxLayout.Y_AXIS));
+		filterPanel.setBounds(FRAME_WIDTH / 2 + 150, 180, FRAME_WIDTH / 2 - 200, FRAME_HEIGHT - 290);
+		filterPanel.setBackground(Color.decode("#fffef7"));
+
 		
+		//Make the filter panel vertically scrollable
+		JScrollPane filterScrollPane = new JScrollPane(filterPanel, 
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+	            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		
+		filterScrollPane.setBounds(FRAME_WIDTH / 2 + 150, 180, FRAME_WIDTH / 2 - 200, FRAME_HEIGHT - 290);
+		filterScrollPane.setBackground(Color.WHITE);
+		add(filterScrollPane);
 	}
 
 
@@ -157,19 +156,11 @@ public class UnemploymentDurationLineChartFrame extends JFrame implements Action
 
 
 
-	public void setBackButton(JButton backButton) {
-		this.backButton = backButton;
-	}
 
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == backButton) {
-            // Handle button click event
-            System.out.println("Back button clicked");
-            new JobspectsTitleFrame();
-            dispose();
-        }
+		
 	}
 
 }
